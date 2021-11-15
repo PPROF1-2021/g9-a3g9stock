3bd8a7d2e1d97e15f66d31a584772ae37c432286
CaroBima <82395515+CaroBima@users.noreply.github.com>
2021-11-15 00:28:09 -0300

    definiendo las clases para registro de usuarios

--- App web ( back + front)/src/java/Logica/Persona.java
+++ App web ( back + front)/src/java/Logica/Persona.java
@@ -1,8 +1,6 @@
 
 package Logica;
 
-import java.util.Date;
-
 
 public class Persona {
     private int idPersona;
@@ -10,7 +8,79 @@ public class Persona {
     private String nombre;
     private String apellido;
     private String telefono;
-    private Date fechaNacimiento;
+    private String email;
     private String direccion;
+    private String provincia;
+
+    public Persona() {
+    }
+
+    public Persona(int idPersona, String nombre, String apellido, String telefono, String email, String direccion, String provincia) {
+        this.idPersona = idPersona;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+        this.email = email;
+        this.direccion = direccion;
+        this.provincia = provincia;
+    }
+
+    public int getIdPersona() {
+        return idPersona;
+    }
+
+    public void setIdPersona(int idPersona) {
+        this.idPersona = idPersona;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getProvincia() {
+        return provincia;
+    }
+
+    public void setProvincia(String provincia) {
+        this.provincia = provincia;
+    }
+    
+    
 }
 
--- App web ( back + front)/src/java/Logica/Usuario.java
+++ App web ( back + front)/src/java/Logica/Usuario.java
@@ -1,8 +1,55 @@
-
+//usuarios del sistema, empleados de la empresa que realizan gestiones sobre el mismo
 
 package Logica;
 
+import java.util.Date;
 
-public class Usuario {
+
+public class Usuario extends Persona{
+    private String contraseña;
+    private Date fechaNacimiento;
+    
+    private TipoDeUsuario tipoUsuario;
+
+    public Usuario() {
+    }
+
+    public Usuario(String contraseña, Date fechaNacimiento, TipoDeUsuario tipoUsuario) {
+        this.contraseña = contraseña;
+        this.fechaNacimiento = fechaNacimiento;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public Usuario(String contraseña, Date fechaNacimiento, TipoDeUsuario tipoUsuario, int idPersona, String nombre, String apellido, String telefono, String email, String direccion, String provincia) {
+        super(idPersona, nombre, apellido, telefono, email, direccion, provincia);
+        this.contraseña = contraseña;
+        this.fechaNacimiento = fechaNacimiento;
+        this.tipoUsuario = tipoUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public TipoDeUsuario getTipoUsuario() {
+        return tipoUsuario;
+    }
+
+    public void setTipoUsuario(TipoDeUsuario tipoUsuario) {
+        this.tipoUsuario = tipoUsuario;
+    }
+    
     
 }
